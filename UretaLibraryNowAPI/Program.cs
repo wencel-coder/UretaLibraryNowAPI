cbd6fa8a55073f56e42e8a9c073c2f7a17ca2c23
wencel-coder <wencel022@gmail.com>
2026-05-09 18:50:06 +0800

    Update Program.cs

--- UretaLibraryNowAPI/Program.cs
+++ UretaLibraryNowAPI/Program.cs
@@ -11,14 +11,12 @@ var app = builder.Build();
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 app.Urls.Add($"http://+:{port}");
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
-app.UseHttpsRedirection();
+
+
 
 app.MapControllers();
-
 app.Run();
